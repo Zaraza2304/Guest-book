98f41843b54ec98ad28460505bb0d450551e302d
Zaraza2304 <edapomidrka5437021polnyi666@gmail.com>
2019-11-21 12:54:23 +0400

    Change DB model

--- model/DB.php
+++ model/DB.php
@@ -1,77 +1,53 @@
 <?php
-//Добавить запросы на создание таблиц
 Class LoadBd {
 
     private $host = '127.0.0.1';
-    private $db_name = 'NewContact';
+    private $db_name = 'Guest_book';
     private $user_name = 'root';
     private $password_db = '';
-    public $status;
 
-    public function connect_db() {
+    private $status_db;
+    public $status_con;
 
-        $answer = mysqli_connect(
-            $this->host,
-            $this->user_name,
-            $this->password_db);
-        $this->status = $answer;
-        if (!$answer) {
-            echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL . "<br>";
-            echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL . "<br>";
-            echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL . "<br>";
 
-            return false;
-        }
+    public function __construct() {
 
-        //echo "Соединение с MySQL установлено!" . PHP_EOL . "<br>";
-        //echo "Информация о сервере: " . mysqli_get_host_info($answer) . PHP_EOL . "<br>";
-        if(!$this->check_db()) {
-            echo "Error, tabels not found and not create!!!<br>";
-            return false;
-        }
+        $this->check_db();
+        $this->check_tabels();
+    }
+
+    private function check_db() {
 
         $answer = mysqli_connect(
             $this->host,
             $this->user_name,
             $this->password_db,
             $this->db_name);
-        $this->status = $answer;
+
         if (!$answer) {
-            echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL . "<br>";
+            /*echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL . "<br>";
             echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL . "<br>";
             echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL . "<br>";
 
-            return false;
-        }
+            return false;*/
 
-        if(!$this->check_tabels()) {
-            return false;
-        }
-        return $answer;
-
-    }
-
-    public function disconnect_db() {
-
-        mysqli_close($this->status);
-
-    }
-
-    private function check_db() {
-
-        $db_selected = mysqli_select_db($this->status, $db_name);
-
-        if (!$db_selected) {
             $sql = 'CREATE DATABASE IF NOT EXISTS NewContact';
 
             if(!mysqli_query($this->status, $sql)) {
-                echo "Error create datbase! " . mysqli_error($this->status) . "<br>";
-                return false;
+                //echo "Error create datbase! " . mysqli_error($this->status) . "<br>";
+                //return false;
+                $this->status_db = false;
+            } else {
+                $this->status_db = true;
             }
-        }  
+        } else {
 
-        $this->disconnect_db();
-        return true;
+            $this->status_db = true;
+            $this->status_con = $answer;
+        }
+
+        mysqli_close($this->status_con);
+        mysqli_close();
     }
 
     private function check_tabels() {
@@ -83,8 +59,9 @@ Class LoadBd {
                         PRIMARY KEY (page_id)
                         )';
         $result = mysqli_query($this->status, $sql_pages);
+
         if (!$result) {
-            echo "Error create table Pages!!! " . mysqli_error($this->status) . "<br>";
+            //echo "Error create table Pages!!! " . mysqli_error($this->status) . "<br>";
             return false;
         }
 
@@ -116,7 +93,7 @@ Class LoadBd {
                         page_id int(11) NOT NULL,
                         user_name varchar(255) NOT NULL,
                         e_mail varchar(255),
-                        text TEXT NOT NULL,
+                        comment TEXT NOT NULL,
                         ip varchar(500) NOT NULL,
                         browser TEXT NOT NULL,
                         home_page TEXT,
@@ -126,13 +103,58 @@ Class LoadBd {
                         FOREIGN KEY (page_id) REFERENCES Pages(page_id) on delete cascade on update cascade,
                         FOREIGN KEY (user_id) REFERENCES Users(id) on delete cascade on update cascade
                         )';
-        
+
         $result = mysqli_query($this->status, $sql_comments);
+
         if(!$result) {
-            echo "Error create table Comments!!! " . mysqli_error($this->status) . "<br>";
+            //echo "Error create table Comments!!! " . mysqli_error($this->status) . "<br>";
             return false;
         }
         return true;
     }
+
+    public function connect_db() {
+
+        /*$answer = mysqli_connect(
+            $this->host,
+            $this->user_name,
+            $this->password_db);
+        $this->status = $answer;
+        if (!$answer) {
+            echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL . "<br>";
+            echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL . "<br>";
+            echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL . "<br>";
+
+            return false;
+        }*/
+
+
+        if(!$this->check_db()) {
+            echo "Error, tabels not found and not create!!!<br>";
+            return false;
+        }
+
+        $answer = mysqli_connect(
+            $this->host,
+            $this->user_name,
+            $this->password_db,
+            $this->db_name);
+        $this->status_con = $answer;
+        if (!$answer) {
+            echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL . "<br>";
+            echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL . "<br>";
+            echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL . "<br>";
+
+            return false;
+        }
+
+        return $answer;
+    }
+
+    public function disconnect_db() {
+
+        mysqli_close($this->status);
+    }
+
 
 }
